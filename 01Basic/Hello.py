d62de9566e37ba3b8416539812319e14ca6f9194
Bachelor <AndyWolfCF2013@163.com>
2020-05-20 22:57:15 +0800

    first notes updating

---
+++ 01Basic/Hello.py
@@ -0,0 +1,12 @@
+#!D:\CODING\Python\python_3.7.2\python.exe
+print("Hello,world!")
+#A line which begin with a '#' means that the line is a comment.
+#以‘#’开头的文字为注释
+#There's no multiply lines comment in python.
+#Python中没有多行注释
+#However,you can use an multiply lines str constant to instead.
+#但可以用多行字符常量来替代
+#On the first line of this file,there is a spacial comment, which point to the python interpreter.
+#文件首行的注释是一个特殊注释，用来指向python解释器
+#It is begin with "#!".
+#以“#!”开头
