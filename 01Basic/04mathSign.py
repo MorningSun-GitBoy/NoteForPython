24ebcf37393545b69db53bd46d3bdba4393521ee
Bachelor <AndyWolfCF2013@163.com>
2020-07-02 15:50:53 +0800

    add math sign 2020 07 02

--- 01Basic/04mathSign.py
+++ 01Basic/04mathSign.py
@@ -1,7 +1,36 @@
 #Addition  加法
 #Using plus sign to make addition
 #使用加号来完成加法
-啊 = 15
-不 = 22
-啊不 = 啊+不
-print(啊不)
+a = 15
+b = 22
+c = a+b
+print(c)
+#Using plus sign to join the string
+#使用加号来连接字符串
+a = "Hallo"
+b = "World"
+c = a + b
+print(c)
+#Subtraction
+#Using subtract sign to make subtraction
+#使用减号做减法
+a = 20
+b = 33
+c = a - b
+pritn(c)
+#Multiplication
+a = 3
+b = 5
+c = a * b
+print(c)
+#Division
+a = 3.0
+b = 5
+c = a / b
+print(c)
+#Modulo
+a = 32
+b = 5
+c = a % b
+print(c)
+#Power
---
+++ 01Basic/05judge.py
@@ -0,0 +1,7 @@
+#if is the judge key
+#if是判断语句关键字
+a = 7
+b = 15.2
+if a < b:
+    print(a+b)
+#there should be a 
