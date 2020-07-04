6ed52865638dc31787cd68bc6edbd52d80808758
Bachelor <AndyWolfCF2013@163.com>
2020-07-04 14:31:51 +0800

    judge 20200704

--- 01Basic/05judge.py
+++ 01Basic/05judge.py
@@ -4,4 +4,15 @@ a = 7
 b = 15.2
 if a < b:
     print(a+b)
-#there should be a 
+#There should be a judge expression behind the key,and using ":" to complete the instruction
+#关键字后跟着判断表达式并用“:”结尾，来完成这个语句
+#the code block behind if instruction do or not is up to the judge expression return what
+#后续代码块是否执行取决于表达式返回什么
+if True :
+    print(True)
+if False :
+    print(a)
+else :
+    print(False)
+#else is another judge key
+#else是另一个关键字
